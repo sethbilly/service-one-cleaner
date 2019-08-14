cfed5af6c8b4fc22a196c844ce26081d38205d4c
Tema Methodist Diocese <methchtemaghdiocese@gmail.com>
2019-08-14 00:04:55 +0000

    Add Test class for optimizationOne

--- src/test/java/com/partner/one/service/ServiceOneApplicationTests.java
+++ src/test/java/com/partner/one/service/ServiceOneApplicationTests.java
@@ -1,16 +1,30 @@
 package com.partner.one.service;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.partner.one.service.api.OptimizerController;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ServiceOneApplicationTests {
+
+	@Autowired
+	private OptimizerController controller;
 
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void testOptimizationOne() {
+		//Testing optimization for
 	}
 
 }
